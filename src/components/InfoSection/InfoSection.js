4e0fb4612de00627dca37434b1750adbc4c6aac6
jsxgod <smykoo7@gmail.com>
2021-01-10 06:29:21 +0100

    Cleaning up unused imports.

--- src/components/InfoSection/InfoSection.js
+++ src/components/InfoSection/InfoSection.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-scroll'
 import { Button, Container } from '../../globalStyles'
 import { InfoColumn, InfoRow, InfoSec, TextWrapper, TopLine, Heading, Subtitle, ImgWrapper, Img } from './InfoSection.elements'
