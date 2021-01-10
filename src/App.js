07692e4088b10df2f0b1b98ab721da678bb30049
jsxgod <smykoo7@gmail.com>
2021-01-10 06:27:12 +0100

    Adding seperate sign up page. Adding script that scrolls newly opened pages to the top.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import GlobalStyle, { Button } from './globalStyles';
+import GlobalStyle from './globalStyles';
 import {Navbar, Footer, SignUp} from './components';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './pages/HomePage/Home';
+import SignUpPage from './pages/SignUp/SignUpPage';
 import { Element } from 'react-scroll';
+import ScrollUp from './components/ScrollUp';
 
 function App() {
 
   return (
     <Router>
       <GlobalStyle />
+      <ScrollUp />
       <Navbar />
       <Switch>
         <Route path="/" exact component={Home}/>
+        <Route path="/sign-up" exact component={SignUpPage}/>
       </Switch>
       <Element name="sign-up">
         <SignUp />
